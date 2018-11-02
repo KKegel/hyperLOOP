6b8119de27d292964cfa42c144ff77ff0db8476c
Karl Kegel <karlkegel9817@gmail.com>
2018-11-02 22:22:03 +0100

    Change tube and light params

--- fancyfarm/src/scene/tube.ts
+++ fancyfarm/src/scene/tube.ts
@@ -12,8 +12,9 @@ class CustomCurve extends THREE.Curve<THREE.Vector3> {
   }
 
   getPoint(t :number) :THREE.Vector3 {
+    
     var tx = t * 3 - 1.5;
-    var ty = Math.sin( 2 * Math.PI * t );
+    var ty = t;//Math.sin( 2 * Math.PI * t );
     var tz = 0;
   
     return new THREE.Vector3( tx, ty, tz ).multiplyScalar( this.scale );
@@ -25,8 +26,8 @@ class CustomCurve extends THREE.Curve<THREE.Vector3> {
 export const tube = () : THREE.Mesh => {
 
   var path :CustomCurve = new CustomCurve( 10 );
-  var geometry :THREE.TubeBufferGeometry = new THREE.TubeBufferGeometry( path, 20, 2, 8, false );
-  var material :THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+  var geometry :THREE.TubeBufferGeometry = new THREE.TubeBufferGeometry( path, 80, 2, 4, false );
+  var material :THREE.MeshLambertMaterial = new THREE.MeshLambertMaterial( { color: 0xaaccff, morphNormals: false } );
   
   return new THREE.Mesh( geometry, material );
 
